e8d586e0259f3fa02f4e1b7777bf78c68ac8d243
Serhii-Sunko <sergeysunko01@gmail.com>
2021-12-08 21:36:12 +0200

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,56 @@
 import './css/styles.css';
-import { fetchCountries } from './js/fetchCountries';
 import { debounce } from 'lodash';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { makeInfo, listOfCountries } from './js/makeMarkup';
 
 const refs = {
   inputRef: document.querySelector('input#search-box'),
   countryList: document.querySelector('.country-list'),
   countryInfo: document.querySelector('.country-info'),
+};
+
+const listOfCountries = countries => {
+  const markup = countries.reduce((acc, country) => {
+    acc += `<li class="countryItem"><img class="countryImage" src="${country.flag}">${country.name}</li>`;
+    return acc;
+  }, '');
+
+  refs.countryList.innerHTML = markup;
+};
+
+const makeInfo = country => {
+  const { name, flag, languages, capital, population } = country;
+
+  const languagesKeys = Object.keys(languages);
+  const languagesMarkup = languagesKeys.map(key => languages[key]).join(', ');
+
+  const markup = `
+  <h1 class="infoTitle"><img class="infoImage" src="${flag}">${name}</h1>
+  <p><span class='label'>Capital: </span><span class='capital'>${capital}</span></p>
+  <p><span class='label'>Population: </span><span class='population'>${population}</span></p>
+  <p><span class='label'>Languages: </span><span class='languages'>${languagesMarkup}</span></p>`;
+
+  refs.countryInfo.innerHTML = markup;
+};
+
+const fetchCountries = name => {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
+  )
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+      return res.json();
+    })
+    .then(res =>
+      res.map(item => ({
+        name: item.name.official,
+        flag: item.flags.svg,
+        capital: item.capital,
+        languages: item.languages,
+        population: item.population,
+      })),
+    );
 };
 
 const DEBOUNCE_DELAY = 300;
